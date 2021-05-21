578e89667f4a379f1f15d1a57a914802a7128d1b
Mohankumar T <mohankumar.tw1@gmail.com>
2021-05-21 21:21:59 +0530

    Add. Colorful printing for error messages

--- guard.sh
+++ guard.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+NOCOLOR='\033[0m'
+RED='\033[0;31m'
 if [ $# == 0 ]
 then
-    echo -e "ERR! Guard expects a path to scan"
+    echo -e "${RED}ERR! Guard expects a path to scan${NOCOLOR}"
     exit 2
 fi
