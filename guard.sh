abe20804eba5431cff2b33e6ebb3a074ae31548c
Mohankumar T <mohankumar.tw1@gmail.com>
2021-05-22 13:39:30 +0530

    Add. Log messages for result of repository scan

--- guard.sh
+++ guard.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 NOCOLOR='\033[0m'
 RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[4;33m'
+
 if [ $# == 0 ]
 then
     echo -e "${RED}ERR! Guard expects a path to scan${NOCOLOR}"
@@ -12,6 +15,9 @@ then
     exit 2
 fi
 
+echo -e "${YELLOW}Scanning for Git Repos inside $1 ............${NOCOLOR}"
 
 gitRepos=$(find $1 -type d -name .git)
 gitRepos=(${gitRepos//".git"/ })
+
+echo -e "${GREEN}Found ${#gitRepos[@]} git repositories.${NOCOLOR}"
